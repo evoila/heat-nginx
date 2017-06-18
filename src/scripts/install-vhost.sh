2bc0809202577985f3162bfca05dc03b170b8e98
Mathias Ewald <mewald@evoila.de>
2017-06-18 09:46:09 +0200

    Feature: Variable evaluation in vhost config

--- src/scripts/install-vhost.sh
+++ src/scripts/install-vhost.sh
@@ -6,10 +6,16 @@ CONFIG=${CONFIG}
 # Make sure is a valid file name
 NAME=$( echo $NAME | sed 's/[^a-zA-Z_-]/_/g' )
 
+# Variables to be used within config
+PRIMARY_ADDR=$(ip addr show dev `ip route show | grep -E "^default" | awk '{ print $5 }'` | grep "inet " | awk '{ print $2 }' | cut -d "/" -f1)
+
+# Eval config to replace variables
+RESULTING_CONFIG=$(eval echo \""$CONFIG"\")
+
 # Write config
 FILE="/etc/nginx/sites-available/$NAME"
 cat <<EOF > $FILE
-$CONFIG
+$RESULTING_CONFIG
 EOF
 
 # Activate vhost
